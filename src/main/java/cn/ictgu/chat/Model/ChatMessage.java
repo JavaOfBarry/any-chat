3d70f9bbdcd5f00b5e2b4c610b34978c28ccc56b
JavaOfBarry <JavaOfBarry@users.noreply.github.com>
2018-05-24 13:51:38 +0800

    1111

--- src/main/java/cn/ictgu/chat/Model/ChatMessage.java
+++ src/main/java/cn/ictgu/chat/Model/ChatMessage.java
@@ -22,42 +22,6 @@ public class ChatMessage {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getSendTime() {
-		return sendTime;
-	}
-
-	public void setSendTime(String sendTime) {
-		this.sendTime = sendTime;
-	}
-
-
+	
 
 }
